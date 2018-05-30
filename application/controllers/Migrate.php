6914928e867c7086cde061d02c4cdc690c4c82ae
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-05-31 06:26:14 +0900

    migration 수동업데이트

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -8,6 +8,9 @@ class Migrate extends CI_Controller
   {
     $this->load->library('migration');
 
+    // print_r($this->migration->find_migrations());
+    // print_r($this->migration->version(20180531041111));
+
     if ($this->migration->current() === FALSE)
     {
       show_error($this->migration->error_string());
