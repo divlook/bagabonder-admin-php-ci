2fdf9acb1656d2a87653f4e06a5f7466f8208220
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-01 05:31:40 +0900

    template file 생성

---
+++ application/views/template/foot.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+    <?php if ($layout['use_full'] === false) { ?>
+      </div>
+    </div>
+    <?php } ?>
+
+    <?php if ($layout['use_nav'] || $layout['use_sidebar']) { ?>
+    <script src="<?php echo base_url(); ?>assets/js/jquery-slim.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <?php } ?>
+
+    <?php if ($layout['use_icon'] === false) { ?>
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+    <?php } ?>
+
+    <?php if ($layout['has_js'] === true) { ?>
+        <script src="<?php echo base_url(); ?>assets/js/<?= $layout['js_name'] ?>.js"></script>
+    <?php } ?>
+
+  </body>
+</html>
