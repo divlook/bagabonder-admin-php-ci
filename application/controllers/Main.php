d93121cd619d1c0739e237fb0e661bedbdc942d4
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-01 05:32:17 +0900

    template 적용

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -12,16 +12,24 @@ class Main extends CI_Controller {
 	public function index()
 	{
 		// echo $this->Detail_info_model->get_test();
+    redirect('dashboard');
 	}
 
   public function dashboard()
   {
-    $this->load->view('dashboard');
+    $data = array();
+    $this->load->view('dashboard', $data);
   }
 
   public function login()
   {
-    $this->load->view('login');
+    $data['layout'] = array(
+      'use_nav' => false,
+      'use_full' => true,
+      'use_sidebar' => false,
+      'use_icon' => false,
+    );
+    $this->load->view('login', $data);
   }
 
 }
