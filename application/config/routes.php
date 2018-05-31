4e7d1317686b8488cd7b0e034d59a1c34366f78b
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-01 05:29:07 +0900

    routes에 dashboard 추가

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = $_ENV['routes']['default_controller'];
+$route['dashboard'] = 'main/dashboard';
 $route['login'] = 'main/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
