12d5804dea902b5aa71debc037277e7b0bbfd237
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-01 00:06:16 +0900

    routes 수정

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = $_ENV['routes']['default_controller'];
+$route['login'] = 'main/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
