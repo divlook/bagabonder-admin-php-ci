7ecfdcbe9dc1cfadd0eaef159715849828079fc8
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-01 18:35:13 +0900

    autoload
    - helper : date
    - libraries : global_lib

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'migration', 'template_lib');
+$autoload['libraries'] = array('database', 'migration', 'template_lib', 'global_lib');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('cookie', 'url');
+$autoload['helper'] = array('cookie', 'url', 'date');
 
 /*
 | -------------------------------------------------------------------
