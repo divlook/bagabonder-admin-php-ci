4e23703954771841d31f81cb1caf5c7eb9c12a05
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-01 13:35:37 +0900

    env.ini에서 서버모드를 설정.

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $_ENV = array_merge($_ENV, file_exists('env.ini') ? parse_ini_file('env.ini', tr
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : $_ENV['server']['CI_ENV']);
 
 /*
  *---------------------------------------------------------------
