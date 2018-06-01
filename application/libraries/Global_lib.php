ef3879f7135426796b9eaf65874b555f0043ee3e
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-01 19:21:22 +0900

    인증 작업중

--- application/libraries/Global_lib.php
+++ application/libraries/Global_lib.php
@@ -25,6 +25,32 @@ class Global_lib {
     $this->CI =& get_instance();
   }
 
+  public function authenticate()
+  {
+    echo 'access_token 체크';
+    // 진입시 가장먼저 실행된다.
+
+    // 세션을 확인한다.
+    $session = $this->input->cookie('app_session');
+
+    // 세션을 생성한다.
+    if ($session === false) $this->input->set_cookie('app_session', 'session');
+
+    // access_token을 확인한다.
+    // auth_model
+
+    // 없다면 access_token을 생성한다.
+    // auth_model
+
+    // access_token이 만료됐다면 로그아웃처리.
+    // auth_model : access_token 삭제
+    // $session 삭제
+
+    // auth와 user의 user_idx를 비교
+    // user_model
+    // auth_model
+  }
+
   public function get_json()
   {
     $json = file_get_contents("php://input");
