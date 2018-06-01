bc847437352a3cbea4f8891ad101b043fb559ff6
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-01 10:21:59 +0900

    필요없는 코드 삭제

--- application/views/login.php
+++ application/views/login.php
@@ -4,7 +4,6 @@ $template = $this->template_lib->layout_parse(@$layout);
 echo $template['head'];
 ?>
 <form class="form-signin">
-    <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
     <label for="inputEmail" class="sr-only">Email address</label>
     <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
