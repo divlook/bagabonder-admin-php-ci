7235ee2c92b3235807e62e1a7f2d6ee3e590b3e7
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-03 20:16:13 +0900

    회사이름과 로그아웃 버튼 추가

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -32,11 +32,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <body class="<?= $layout['use_full'] === true ? 'text-center' : '' ?>">
     <?php if ($layout['use_nav'] === true) { ?>
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"><?= $_ENV['head']['company'] ?></a>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
+          <a class="nav-link" href="/logout">Sign out</a>
         </li>
       </ul>
     </nav>
