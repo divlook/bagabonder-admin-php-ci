d77d266e1a3e0e6866862b31a96b0d145fc66f61
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-03 15:12:06 +0900

    login, logout api

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -29,7 +29,7 @@ class User extends CI_Controller {
     if ($username_overlap === false) {
       $post_result = $this->user_model->user_post(array(
         'username' => $json->username,
-        'password' => hash('sha256', $json->password . $_ENV['security']['salt']),
+        'password' => $this->global_lib->generate_password($json->password),
         'level' => $json->level,
       ));
       if ($post_result) {
@@ -42,6 +42,55 @@ class User extends CI_Controller {
     }
 
     $this->global_lib->result2json($result);
+  }
+
+  public function login()
+  {
+    if ($this->input->method(TRUE) === 'GET') {
+      show_404();
+      return;
+    }
+    $json = $this->global_lib->get_json();
+
+    $result = array('code' => 1);
+
+    $access_token = $this->global_lib->generate_access_token();
+
+    $user_data = $this->user_model->get_user_data(array(
+      'username' => $json->username,
+    ));
+
+    if (!$user_data) {
+      $result['code'] = 5;
+      $result['msg'] = 'username';
+    }
+
+    if ($json->password !== $this->global_lib->generate_password($json->password)) {
+      $result['code'] = 3;
+      $result['msg'] = 'password';
+    }
+
+    $this->input->set_cookie('user_idx', $user_data->idx, 0);
+
+    $this->auth_model->set_auth(array(
+      'user_idx' => $user_data->idx,
+      'access_token' => $access_token,
+    ));
+
+    $this->global_lib->result2json($result);
+  }
+
+  public function logout()
+  {
+    $access_token =$this->global_lib->generate_access_token();
+
+    $this->auth_model->del_auth(array(
+      'access_token' => $access_token,
+      'user_idx' => $this->input->cookie('user_idx'),
+    ));
+
+    $this->input->set_cookie('app_session', '');
+    $this->input->set_cookie('user_idx', '');
   }
 
   public function _private_method()
