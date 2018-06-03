2cedef0426488ed861d351d788b7105f39074279
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-03 19:31:03 +0900

    url을 join하는 함수 추가

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
 ;(function () {
-    var app = function() {
-        this.hello = function () {
-            console.log('hello')
-            return 'hello'
-        }
+  var app = function() {
+    this.hello = function () {
+      console.log('hello')
+      return 'hello'
     }
-    window.app = new app()
+    this.url = {
+      join: function () {
+        var url = app_config.base_url
+        for (var i = 0, len = arguments.length; i < len; i++) {
+          var path = arguments[i]
+          if (!/^\//.test(path)) path = '/' + path
+          path.replace(/\/$/, '')
+          url += path
+        }
+        return url
+      }
+    }
+  }
+  window.app = new app()
 })()
