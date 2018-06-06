7ca85d6ecbded7c7233ccb5eb520b85c9496377a
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-06-07 07:54:33 +0900

    return_url 작업

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $template = $this->template_lib->layout_parse(@$layout);
 echo $template['head'];
 ?>
-<form class="form-signin">
+  <form class="form-signin">
+    <input type="hidden" id="input-return-url" value="<?= $this->input->get('return_url') ?>">
     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
     <label for="input-username" class="sr-only">Username</label>
     <input type="text" id="input-username" class="form-control" placeholder="Username" required autofocus>
@@ -11,6 +12,6 @@ echo $template['head'];
     <input type="password" id="input-password" class="form-control" placeholder="Password" required>
 
     <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-</form>
+    <p class="mt-5 mb-3 text-muted">&copy; 2018</p>
+  </form>
 <?php echo $template['foot']; ?>
--- assets/js/app_login.js
+++ assets/js/app_login.js
@@ -1,14 +1,11 @@
 ;(function () {
 
-  // 로그인
   var login_form = document.querySelector('.form-signin')
-  login_form.addEventListener('submit', function(event) {
-    event.stopPropagation()
-    event.preventDefault()
+  var username = login_form.querySelector('#input-username')
+  var password = login_form.querySelector('#input-password')
+  var return_url = login_form.querySelector('#input-return-url')
 
-    var username = login_form.querySelector('#input-username')
-    var password = login_form.querySelector('#input-password')
-
+  var login_callback = function(event) {
     if (!username.value || !password.value) {
       return false
     }
@@ -21,7 +18,7 @@
 
       switch (result.code) {
         case 1:
-          location.href = app.url.join('dashboard')
+          location.href = return_url.value ? app.url.join(return_url.value) : app.url.join('dashboard')
           break
         case 3:
           alert('password 틀림')
@@ -36,6 +33,7 @@
           break
       }
     })
-  })
+  }
 
+  login_form.addEventListener('submit', app.debounce(login_callback, 300, true))
 })()
